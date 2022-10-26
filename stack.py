91d59bc341cda1538321316dbba09e87c38fa60c
rockz007 <rockzmurali007@gmail.com>
2022-10-26 11:39:04 +0530

    f

---
+++ stack.py
@@ -0,0 +1,39 @@
+top=0
+mymax=eval(input("enter maximum size of stack:"))
+def createstack():
+    stack=[]
+    return stack
+def isempty(stack):
+ return(lenstack)==0
+def push(stack,item):
+    stack.append(item)
+    print("pushed to stack",item)
+    def pop(stack):
+  if is Empty(Stack);
+    return "stack underflow"
+return stack.pop()
+stack=create stack()
+while true:
+    print("1.push")
+    
+ print("2.pop")
+ 
+ print("3.Display")
+ 
+ print("4.quit")
+ ch=int ((input("Enter your choice:"))
+ if ch==1:
+     if top < mymax:
+         item=input("Enter any element:")
+         push(stack,item)
+         top+=1
+         else:
+             print("stack overflow")
+              elif ch==2:
+         print("pop(stack)")
+         elif ch==3:
+         print("stack")
+         else:
+             print("Exit")
+             break;
+         
